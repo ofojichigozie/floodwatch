5ea842458a37c771100dd46c4e4269165c046af3
Chigozie Ofoji <ofojichigozie@gmail.com>
2026-06-20 09:13:13 +0100

    Setup AI service for prod

--- floodwatch-ai/app.py
+++ floodwatch-ai/app.py
@@ -20,4 +20,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.getenv("PORT", "8000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
